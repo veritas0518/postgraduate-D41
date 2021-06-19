3c5c019a85f178b86bd2d87630ecd215707278c6
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-19 21:53:06 +0800

    Cylinder class  is derived from circle class.
    【问题描述】定义圆类Circle，具有描述半径的成员变量radius，具有无参构造方法和一个参数的构造方法，计算周长的成员方法getPerimeter()，计算面积的成员方法getArea()。
    从圆类Circle派生出圆柱类Cylinder，添加描述圆柱体高度的成员变量height，一个参数的构造方法，计算圆柱表面积的成员方法getSurfaceArea()，计算体积的成员方法getVol()。
    在Test类的main()方法中，创建圆类和圆柱类的对象，从键盘输入圆的半径，圆柱体的底面半径和高，计算并显示圆的面积和周长，圆柱的表面积和体积。所有输出结果，小数点后保留3位。
    【样例输入1】
    5
    3 8
    【样例输出1】
    Perimeter:31.416
    Area:78.540
    Surface area:207.345
    Volume:226.195
    
    【样例输入2】
    3.10
    6.28 9.21
    【样例输出2】
    Perimeter:19.478
    Area:30.191
    Surface area:611.211
    Volume:1141.113

---
+++ TestCircle.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.math.*;
+public class TestCircle {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double radius1=s.nextDouble();
+        Circle c1=new Circle(radius1);
+
+        double radius=s.nextDouble();
+        double height=s.nextDouble();
+        Cylinder c2=new Cylinder(radius,height);
+        System.out.printf("Perimeter:%f\n",c1.getPerimeter());
+        System.out.printf("Area:%f\n",c1.getArea());
+        System.out.printf("Surface area:%f\n",c2.getSurfaceArea());
+        System.out.printf("Volume:%f\n",c2.getVol());
+
+    }
+}
+class Circle{
+    double radius;
+    Circle(){}
+    Circle(double radius){
+        this.radius=radius;
+    }
+    public double getPerimeter(){return Math.PI*2*radius;}
+    public double getArea(){return Math.PI*radius*radius;}
+}
+class Cylinder extends Circle {
+    double height;
+
+    Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    public double getSurfaceArea() {
+        return getArea() * 2 + getPerimeter() * height;
+    }
+
+    public double getVol() {
+        return getArea() * height;
+    }
+
+}
